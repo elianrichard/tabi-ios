03898aec37eb6a2f332ee99b2de55e2592573605
Darma <darmawanruslan12@gmail.com>
2024-10-08 12:39:57 +0700

    Moved variables to viewmodel

--- Tabi/Modules/Expenses/AddExpenseView.swift
+++ Tabi/Modules/Expenses/AddExpenseView.swift
@@ -10,22 +10,7 @@ import SwiftUI
 import PhotosUI
 
 struct AddExpenseView: View {
-    @State var eventName: String
-    @State var peoples: [People] = [
-        People(name: "Dharma"),
-        People(name: "Mario"),
-        People(name: "Renaldi"),
-        People(name: "Leo"),
-    ]
-    @State var expense: Expense = Expense(name: "Sate", coverer: "Naufal", dateOfCreation: .now, price: 100000)
-    @State var expenseName: String = ""
-    @State var selectedParticipants: [People] = []
-    @State var methods: [String] = ["Split Equally", "Custom"]
-    @State var selectedMethod: String = ""
-    @State var selectedCoverer: People?
-    @State var gridItem: [GridItem] = []
-    @State var expenseTotal: Int?
-    @State var receiptImage: PhotosPickerItem? = nil
+    @State var viewModel: AddExpenseViewModel = AddExpenseViewModel()
     @EnvironmentObject var routes: Routes
     
     var body: some View {
@@ -35,7 +20,7 @@ struct AddExpenseView: View {
                 VStack(alignment: .leading){
                     Text("Title")
                         .padding([.top, .bottom], 5)
-                    TextField("Title", text: $expenseName)
+                    TextField("Title", text: $viewModel.expenseName)
                         .padding(10)
                         .background(Color(.midLightGray))
                         .cornerRadius(5)
@@ -44,16 +29,16 @@ struct AddExpenseView: View {
                     Text("Paid by")
                         .padding([.top, .bottom], 5)
                     Menu {
-                        ForEach(peoples, id: \.id) { people in
+                        ForEach(viewModel.peoples, id: \.id) { people in
                             Button(people.name, action: {
-                                selectedCoverer = people
+                                viewModel.selectedCoverer = people
                             })
                             .frame(maxWidth: .infinity)
                         }
                     } label: {
                         HStack{
-                            if selectedCoverer != nil{
-                                Text(selectedCoverer!.name)
+                            if viewModel.selectedCoverer != nil{
+                                Text(viewModel.selectedCoverer!.name)
                             }else{
                                 Text("Paid by")
                             }
@@ -70,8 +55,8 @@ struct AddExpenseView: View {
                 VStack(alignment: .leading){
                     Text("Participants")
                         .padding([.top, .bottom], 5)
-                    LazyVGrid(columns: gridItem, spacing: 16){
-                        ForEach(peoples, id: \.id) { people in
+                    LazyVGrid(columns: viewModel.gridItem, spacing: 16){
+                        ForEach(viewModel.peoples, id: \.id) { people in
                             VStack{
                                 Circle()
                                     .frame(width: 40, height: 40)
@@ -79,18 +64,18 @@ struct AddExpenseView: View {
                                         Circle()
                                             .frame(width: 50, height: 50)
                                             .foregroundColor(.gray)
-                                            .opacity(!selectedParticipants.contains(people) ? 0 : 0.5)
+                                            .opacity(!viewModel.selectedParticipants.contains(people) ? 0 : 0.5)
                                     }
                                 Text(people.name)
                                     .font(.subheadline)
                                     .lineLimit(1)
                             }
                             .onTapGesture {
-                                if !selectedParticipants.contains(people){
-                                    selectedParticipants.append(people)
+                                if !viewModel.selectedParticipants.contains(people){
+                                    viewModel.selectedParticipants.append(people)
                                 }else{
-                                    let removeIndex = selectedParticipants.firstIndex(of: people)
-                                    selectedParticipants.remove(at: removeIndex!)
+                                    let removeIndex = viewModel.selectedParticipants.firstIndex(of: people)
+                                    viewModel.selectedParticipants.remove(at: removeIndex!)
                                 }
                             }
                         }
@@ -101,23 +86,23 @@ struct AddExpenseView: View {
                     )
                     .cornerRadius(5)
                     .onAppear{
-                        gridItem = Array(repeating: .init(.flexible()), count: 5)
+                        viewModel.gridItem = Array(repeating: .init(.flexible()), count: 5)
                     }
                 } // Participants
                 VStack(alignment: .leading){
                     Text("Split Method")
                         .padding([.top, .bottom], 5)
                     Menu {
-                        ForEach(methods, id: \.self) { method in
+                        ForEach(viewModel.methods, id: \.self) { method in
                             Button(method, action: {
-                                selectedMethod = method
+                                viewModel.selectedMethod = method
                             })
                             .frame(maxWidth: .infinity)
                         }
                     } label: {
                         HStack{
-                            if selectedMethod != ""{
-                                Text(selectedMethod)
+                            if viewModel.selectedMethod != ""{
+                                Text(viewModel.selectedMethod)
                             }else{
                                 Text("Split Method")
                             }
@@ -131,13 +116,13 @@ struct AddExpenseView: View {
                         .cornerRadius(5)
                     }
                 } // Split Method
-                if selectedMethod == "Split Equally"{
+                if viewModel.selectedMethod == "Split Equally"{
                     VStack(alignment: .leading){
                         Text("Total Bill (RP)")
                             .padding([.top, .bottom], 5)
                         HStack{
                             Text("Rp")
-                            TextField("0", value: $expenseTotal, format: .number .grouping(.automatic)
+                            TextField("0", value: $viewModel.expenseTotal, format: .number .grouping(.automatic)
                             )
                             .keyboardType(.numberPad)
                         }
@@ -149,7 +134,7 @@ struct AddExpenseView: View {
                 VStack(alignment: .leading){
                     Text("Payment Receipt")
                         .padding([.top, .bottom], 5)
-                    PhotosPicker(selection: $receiptImage, matching: .images, photoLibrary: .shared()){
+                    PhotosPicker(selection: $viewModel.receiptImage, matching: .images, photoLibrary: .shared()){
                         VStack(spacing: 10){
                             Image(systemName: "photo")
                                 .resizable()
@@ -180,5 +165,5 @@ struct AddExpenseView: View {
 }
 
 #Preview {
-    AddExpenseView(eventName: "Japan Trip")
+    AddExpenseView()
 }
