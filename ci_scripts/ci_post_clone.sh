5414e155f759dcd4528cadac38c00a897a8e3ab5
elianrichard <elianrichards@gmail.com>
2024-10-11 10:28:17 +0700

    escape space for accessing tabi rpoject

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -12,37 +12,37 @@ cd ..
 echo "Generating Xcode project..."
 xcodegen
 
-if [ ! -d "Tabi Split.xcodeproj" ]; then
-    echo "Error: Tabi Split.xcodeproj not found!"
+if [ ! -d "Tabi\ Split.xcodeproj" ]; then
+    echo "Error: Tabi\ Split.xcodeproj not found!"
     exit 1
 fi
 
-if [ ! -d "Tabi Split.xcodeproj/project.xcworkspace" ]; then
+if [ ! -d "Tabi\ Split.xcodeproj/project.xcworkspace" ]; then
     echo "Error: project.xcworkspace not found!"
     exit 1
 fi
 
 echo "Checking xcshareddata directory..."
 
-if [ ! -d "Tabi Split.xcodeproj/project.xcworkspace/xcshareddata" ]; then
+if [ ! -d "Tabi\ Split.xcodeproj/project.xcworkspace/xcshareddata" ]; then
     echo "Creating xcshareddata directory..."
-    mkdir -p Tabi Split.xcodeproj/project.xcworkspace/xcshareddata
+    mkdir -p Tabi\ Split.xcodeproj/project.xcworkspace/xcshareddata
 fi
 
-if [ ! -d "Tabi Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm" ]; then
+if [ ! -d "Tabi\ Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm" ]; then
     echo "Creating swiftpm directory..."
-    mkdir -p Tabi Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
+    mkdir -p Tabi\ Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
 fi
 
-if [ ! -f "Tabi Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
+if [ ! -f "Tabi\ Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
     echo "Creating Package.resolved file..."
-    touch Tabi Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
+    touch Tabi\ Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
 fi
 
 echo "Resolving package dependencies..."
-xcodebuild -resolvePackageDependencies -project Tabi Split.xcodeproj -scheme Tabi Split
+xcodebuild -resolvePackageDependencies -project Tabi\ Split.xcodeproj -scheme Tabi\ Split
 
-if [ -f "Tabi Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
+if [ -f "Tabi\ Split.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" ]; then
     echo "Package.resolved generated successfully."
 else
     echo "Failed to generate Package.resolved."
