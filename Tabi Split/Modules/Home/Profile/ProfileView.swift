88d73b68ca93a7d72ce3142d2e554a4ce30f5cca
Ahmad Naufal Alfakhar <98883555+naufalfakhar@users.noreply.github.com>
2024-11-07 15:41:37 +0700

    [FEAT] logout (#40)

--- Tabi Split/Modules/Home/Profile/ProfileView.swift
+++ Tabi Split/Modules/Home/Profile/ProfileView.swift
@@ -61,13 +61,25 @@ struct ProfileView: View {
                         routes.navigate(to: .PaymentMethods)
                     }
                     Divider()
-                    HStack(spacing: .spacingTight){
-                        Icon(.logout, size: 20)
-                        Text("Log Out")
-                            .font(.tabiHeadline)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .frame(width: 24, height: 24)
+                    Button {
+                        Task {
+                            await profileViewModel.logout()
+                            
+                            if profileViewModel.isLogoutSuccess {
+                                routes.navigate(to: .LoginView)
+                            }
+                        }
+                    } label: {
+                        HStack(spacing: .spacingTight){
+                            Icon(.logout, size: 20)
+                            Text("Log Out")
+                                .font(.tabiHeadline)
+                                .foregroundStyle(.black)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .frame(width: 24, height: 24)
+                                .foregroundStyle(.black)
+                        }
                     }
                     Divider()
                 }
