0a310b0424d1508501142328ba769ad51b0eff99
elianrichard <elianrichards@gmail.com>
2024-11-07 21:29:33 +0700

    reverted content view auth

--- Tabi Split/ContentView.swift
+++ Tabi Split/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack (path: $routes.navPath) {
             VStack {
-                if !isAuthenticated {
+                if isAuthenticated {
                     HomeView()
                 } else {
                     LoginView()
